b46916cb2139efa8614e4dd39172290cd3c128da
Manish Anand <manishanandprogrammer@gmail.com>
2021-06-12 05:31:17 +0530

    added webclient spport to project

--- src/main/java/com/example/controller/MovieCatalogController.java
+++ src/main/java/com/example/controller/MovieCatalogController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,9 +18,12 @@ import java.util.stream.Collectors;
 public class MovieCatalogController {
 
     private final RestTemplate restTemplate;
+    private final WebClient.Builder webClientBuilder;
 
-    public MovieCatalogController(final RestTemplate restTemplate) {
+    public MovieCatalogController(final RestTemplate restTemplate,
+                                  final WebClient.Builder webClientBuilder) {
         this.restTemplate = restTemplate;
+        this.webClientBuilder = webClientBuilder;
     }
 
     @GetMapping("/{userId}")
@@ -32,5 +36,21 @@ public class MovieCatalogController {
                     Movie movie = restTemplate.getForObject("http://localhost:8081/movie/" + rating.getMovieId(), Movie.class);
                     return new CatalogItem(movie.getName(), movie.getId(), rating.getRating());
                 }).collect(Collectors.toList());
+    }
+
+    @GetMapping("/webclient/{userId}")
+    public List<CatalogItem> movieCatalogsWithWebClient(@PathVariable("userId") final String userId) {
+        List<Rating> ratings = List.of(new Rating("a", 4), new Rating("b", 5));
+
+        return ratings.stream()
+                    .map(rating -> {
+                        Movie movie = webClientBuilder.build()
+                                .get()
+                                .uri("http://localhost:8081/movie/" + rating.getMovieId())
+                                .retrieve()
+                                .bodyToMono(Movie.class)
+                                .block();
+                        return new CatalogItem(movie.getName(), movie.getId(), rating.getRating());
+                    }).collect(Collectors.toList());
     }
 }
